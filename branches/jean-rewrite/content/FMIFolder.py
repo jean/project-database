1c9bc6b4e90fd358a2c73afc28b4bb8e12c7da4f
afkruger <afkruger@61ed036f-b72b-0410-9ea5-b9ec1d72d98d>
2007-10-25 19:19:40 +0000

    Bug/UI fixes

--- branches/jean-rewrite/content/FMIFolder.py
+++ branches/jean-rewrite/content/FMIFolder.py
@@ -100,6 +100,13 @@ class FMIFolder(BaseFolder):
     schema = FMIFolder_schema
 
     ##code-section class-header #fill in your manual code here
+    actions = actions + ({  'action':"string:${object_url}/edit",
+                            'category':"object",
+                            'id':'edit',
+                            'name':'edit',
+                            'permissions':('Modify portal content'),
+                            'condition':'python:0',
+                        },)
     ##/code-section class-header
 
     # Methods
