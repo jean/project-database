658ea0db3f76d2349b40b62d8902189d34ba2874
jurgen.blignaut <jurgen.blignaut@61ed036f-b72b-0410-9ea5-b9ec1d72d98d>
2008-11-11 14:57:18 +0000

    Refining main template and adding a viewlet for the main menu

--- unep.theme/trunk/unep.theme/unep/theme/browser/interfaces.py
+++ unep.theme/trunk/unep.theme/unep/theme/browser/interfaces.py
@@ -1,5 +1,18 @@
+from zope.interface import Interface
 from plone.theme.interfaces import IDefaultPloneLayer
 
 class IThemeSpecific(IDefaultPloneLayer):
     """Marker interface that defines a Zope 3 browser layer.
     """
+
+class IUnepViewUtils(Interface):
+    """ Marker interface for the UNEP main browser utility view class
+    """
+
+    def projectDatabasesURL(self):
+        """ return the URL of the project databases folder
+        """
+
+    def contactsURL(self):
+        """ return the URL of the contacts manager
+        """
