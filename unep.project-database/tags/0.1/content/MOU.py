97585cf46bde2f47004041bd9f9d368b54f8e10a
jurgen.blignaut <jurgen.blignaut@61ed036f-b72b-0410-9ea5-b9ec1d72d98d>
2008-11-14 12:11:13 +0000

    Created a 0.1 tag for project database

---
+++ unep.project-database/tags/0.1/content/MOU.py
@@ -0,0 +1,78 @@
+# -*- coding: utf-8 -*-
+#
+# File: MOU.py
+#
+# Copyright (c) 2008 by []
+# Generator: ArchGenXML Version 2.0
+#            http://plone.org/products/archgenxml
+#
+# GNU General Public License (GPL)
+#
+
+__author__ = """Jean Jordaan <jean.jordaan@gmail.com>, Jurgen Blignaut
+<jurgen.blignaut@gmail.com>"""
+__docformat__ = 'plaintext'
+
+from AccessControl import ClassSecurityInfo
+from Products.Archetypes.atapi import *
+from zope.interface import implements
+import interfaces
+from Products.ProjectDatabase.content.SubProject import SubProject
+from Products.CMFDynamicViewFTI.browserdefault import BrowserDefaultMixin
+
+from Products.ATVocabularyManager.namedvocabulary import NamedVocabulary
+from Products.ProjectDatabase.config import *
+
+# additional imports from tagged value 'import'
+from Products.FinanceFields.MoneyField import MoneyField
+from Products.FinanceFields.MoneyWidget import MoneyWidget
+from Products.DataGridField import DataGridField, DataGridWidget, Column, SelectColumn, CalendarColumn
+from Products.ATReferenceBrowserWidget.ATReferenceBrowserWidget import ReferenceBrowserWidget
+import Project
+import Financials
+from Products.CMFCore.utils import getToolByName
+from Products.FinanceFields.Money import Money
+
+##code-section module-header #fill in your manual code here
+##/code-section module-header
+
+schema = Schema((
+
+),
+)
+
+##code-section after-local-schema #fill in your manual code here
+##/code-section after-local-schema
+
+MOU_schema = BaseFolderSchema.copy() + \
+    getattr(SubProject, 'schema', Schema(())).copy() + \
+    schema.copy()
+
+##code-section after-schema #fill in your manual code here
+##/code-section after-schema
+
+class MOU(BaseFolder, SubProject, BrowserDefaultMixin):
+    """
+    """
+    security = ClassSecurityInfo()
+    implements(interfaces.IMOU)
+
+    meta_type = 'MOU'
+    _at_rename_after_creation = True
+
+    schema = MOU_schema
+
+    ##code-section class-header #fill in your manual code here
+    ##/code-section class-header
+
+    # Methods
+
+
+registerType(MOU, PROJECTNAME)
+# end of class MOU
+
+##code-section module-footer #fill in your manual code here
+##/code-section module-footer
+
+
+
---
+++ unep.project-database/tags/0.1/setuphandlers.py
@@ -0,0 +1,168 @@
+# -*- coding: utf-8 -*-
+#
+# File: setuphandlers.py
+#
+# Copyright (c) 2008 by []
+# Generator: ArchGenXML Version 2.0
+#            http://plone.org/products/archgenxml
+#
+# GNU General Public License (GPL)
+#
+
+__author__ = """Jean Jordaan <jean.jordaan@gmail.com>, Jurgen Blignaut
+<jurgen.blignaut@gmail.com>"""
+__docformat__ = 'plaintext'
+
+
+import logging
+logger = logging.getLogger('ProjectDatabase: setuphandlers')
+from Products.ProjectDatabase.config import PROJECTNAME
+from Products.ProjectDatabase.config import DEPENDENCIES
+import os
+from config import product_globals
+from Globals import package_home
+from Products.ATVocabularyManager.config import TOOL_NAME as ATVOCABULARYTOOL
+from Products.CMFCore.utils import getToolByName
+import transaction
+##code-section HEAD
+##/code-section HEAD
+
+def isNotProjectDatabaseProfile(context):
+    return context.readDataFile("ProjectDatabase_marker.txt") is None
+
+def installVocabularies(context):
+    """creates/imports the atvm vocabs."""
+    if isNotProjectDatabaseProfile(context): return 
+    shortContext = context._profile_path.split(os.path.sep)[-3]
+    if shortContext != 'ProjectDatabase': # avoid infinite recursions
+        return
+    site = context.getSite()
+    # Create vocabularies in vocabulary lib
+    atvm = getToolByName(site, ATVOCABULARYTOOL)
+    vocabmap = {'ProjectStatusses': ('VdexVocabulary', 'SimpleVocabularyTerm'),
+         'Category': ('VdexVocabulary', 'SimpleVocabularyTerm'),
+         'StrategicPriority': ('VdexVocabulary', 'SimpleVocabularyTerm'),
+         'GEFPhase': ('VdexVocabulary', 'SimpleVocabularyTerm'),
+         'ProjectCycleStage': ('VdexVocabulary', 'SimpleVocabularyTerm'),
+         'Scope': ('VdexVocabulary', 'SimpleVocabularyTerm'),
+         'ReviewEvaluation': ('VdexVocabulary', 'SimpleVocabularyTerm'),
+         'Assessment': ('VdexVocabulary', 'SimpleVocabularyTerm'),
+         'Status': ('VdexVocabulary', 'SimpleVocabularyTerm'),
+         'ImplementationMode': ('VdexVocabulary', 'SimpleVocabularyTerm'),
+         'ProjectType': ('VdexVocabulary', 'SimpleVocabularyTerm'),
+         'MilestoneName': ('VdexVocabulary', 'SimpleVocabularyTerm'),
+         'ImplementationStatus': ('VdexVocabulary', 'SimpleVocabularyTerm'),
+         'EAPOP': ('VdexVocabulary', 'SimpleVocabularyTerm'),
+         'OperationalProgramme': ('VdexVocabulary', 'SimpleVocabularyTerm'),
+         'FinanceCategory': ('VdexVocabulary', 'SimpleVocabularyTerm'),
+         'PipelineNumber': ('VdexVocabulary', 'SimpleVocabularyTerm'),
+         'Country': ('VdexVocabulary', 'SimpleVocabularyTerm'),
+         'Region': ('VdexVocabulary', 'SimpleVocabularyTerm'),
+         'EABiodiversity': ('VdexVocabulary', 'SimpleVocabularyTerm'),
+         'ProjectRevisionType': ('VdexVocabulary', 'SimpleVocabularyTerm'),
+         'MultipleFocalAreas': ('VdexVocabulary', 'SimpleVocabularyTerm'),
+         'FocalArea': ('VdexVocabulary', 'SimpleVocabularyTerm'),
+         'StrategicObjectives': ('VdexVocabulary', 'SimpleVocabularyTerm'),
+         'InceptionRiskRating': ('VdexVocabulary', 'SimpleVocabularyTerm'),
+         'Number': ('VdexVocabulary', 'SimpleVocabularyTerm'),
+         'EAClimateChange': ('VdexVocabulary', 'SimpleVocabularyTerm'),
+         'StrategicProgram': ('VdexVocabulary', 'SimpleVocabularyTerm'),
+         'LeadAgency': ('VdexVocabulary', 'SimpleVocabularyTerm'),
+        }
+    for vocabname in vocabmap.keys():
+        if not vocabname in atvm.contentIds():
+            atvm.invokeFactory(vocabmap[vocabname][0], vocabname)
+
+        if len(atvm[vocabname].contentIds()) < 1:
+            if vocabmap[vocabname][0] == "VdexVocabulary":
+                vdexpath = os.path.join(
+                    package_home(product_globals), 'data', '%s.vdex' % vocabname)
+                if not (os.path.exists(vdexpath) and os.path.isfile(vdexpath)):
+                    logger.warn('No VDEX import file provided at %s.' % vdexpath)
+                    continue
+                try:
+                    #read data
+                    f = open(vdexpath, 'r')
+                    data = f.read()
+                    f.close()
+                except:
+                    logger.warn("Problems while reading VDEX import file "+\
+                                "provided at %s." % vdexpath)
+                    continue
+                # this might take some time!
+                atvm[vocabname].importXMLBinding(data)
+            else:
+                pass
+
+
+
+def updateRoleMappings(context):
+    """after workflow changed update the roles mapping. this is like pressing
+    the button 'Update Security Setting' and portal_workflow"""
+    if isNotProjectDatabaseProfile(context): return 
+    shortContext = context._profile_path.split(os.path.sep)[-3]
+    if shortContext != 'ProjectDatabase': # avoid infinite recursions
+        return
+    wft = getToolByName(context.getSite(), 'portal_workflow')
+    wft.updateRoleMappings()
+
+def postInstall(context):
+    """Called as at the end of the setup process. """
+    # the right place for your custom code
+    if isNotProjectDatabaseProfile(context): return
+    shortContext = context._profile_path.split(os.path.sep)[-3]
+    if shortContext != 'ProjectDatabase': # avoid infinite recursions
+        return
+    site = context.getSite()
+
+
+
+##code-section FOOT
+def installDataGridVocabularies(context):
+    """ Install additional vocabularies needed by datagrid fields
+        but that ArchenGenXML does not know about. """
+    if isNotProjectDatabaseProfile(context): return 
+    shortContext = context._profile_path.split(os.path.sep)[-3]
+    if shortContext != 'ProjectDatabase': # avoid infinite recursions
+        return
+    site = context.getSite()
+    # Create vocabularies in vocabulary lib
+    atvm = getToolByName(site, ATVOCABULARYTOOL)
+    vocabmap = {
+        'OtherRatings': ('VdexVocabulary', 'SimpleVocabularyTerm'),
+        'YesOrNo': ('VdexVocabulary', 'SimpleVocabularyTerm'),
+        'Rating': ('VdexVocabulary', 'SimpleVocabularyTerm'),
+        'EORatingElements': ('VdexVocabulary', 'SimpleVocabularyTerm'),
+        'AgencyImplementation': ('VdexVocabulary', 'SimpleVocabularyTerm'),
+        'DonorType': ('VdexVocabulary', 'SimpleVocabularyTerm'),
+        'ReportType': ('VdexVocabulary', 'SimpleVocabularyTerm'),
+        'MEMilestoneName': ('VdexVocabulary', 'SimpleVocabularyTerm'),
+        'EvaluationTypeMilestone': ('VdexVocabulary', 'SimpleVocabularyTerm'),
+        'ApprovalInitiationClosure': ('VdexVocabulary', 'SimpleVocabularyTerm'),
+        'ProjectProcesses': ('VdexVocabulary', 'SimpleVocabularyTerm'),
+        }
+    for vocabname in vocabmap.keys():
+        if not vocabname in atvm.contentIds():
+            atvm.invokeFactory(vocabmap[vocabname][0], vocabname)
+
+        if len(atvm[vocabname].contentIds()) < 1:
+            if vocabmap[vocabname][0] == "VdexVocabulary":
+                vdexpath = os.path.join(
+                    package_home(product_globals), 'data', '%s.vdex' % vocabname)
+                if not (os.path.exists(vdexpath) and os.path.isfile(vdexpath)):
+                    logger.warn('No VDEX import file provided at %s.' % vdexpath)
+                    continue
+                try:
+                    #read data
+                    f = open(vdexpath, 'r')
+                    data = f.read()
+                    f.close()
+                except:
+                    logger.warn("Problems while reading VDEX import file "+\
+                                "provided at %s." % vdexpath)
+                    continue
+                # this might take some time!
+                atvm[vocabname].importXMLBinding(data)
+            else:
+                pass
+##/code-section FOOT
