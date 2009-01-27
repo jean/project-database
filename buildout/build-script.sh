573b61f7d97f9faa2c3931f14f9838edf89bd6af
mikejmets <mikejmets@61ed036f-b72b-0410-9ea5-b9ec1d72d98d>
2009-01-27 14:25:37 +0000

    Fixed buid-site script

--- buildout/build-script.sh
+++ buildout/build-script.sh
@@ -1,11 +1,32 @@
+#!/bin/bash
+#Assumptions:
+# The current user home folder contains:
+#   repos/google/project-database/...
+#   instances folder (WITHOUT a folder called unep)
+#   PIL is untarred in src/Imaging-1.1.6 
+
+echo
+echo "=======================Setup python"
 cd ~/instances
-mkdir unep_project
-cd unep_project
-virtualenv --no-site-packages
-source python/bin/activate
+virtualenv --no-site-packages unep
+source unep/bin/activate
+python ~/src/Imaging-1.1.6/setup.py install
+echo
+echo "=======================Setup plone"
 easy_install ZopeSkel
 paster create -t plone3_buildout unep
+echo
+echo "=======================Setup buildout"
 rm unep/buildout.cfg
 ln -s ~/repos/google/project-database/buildout/develop/buildout.cfg unep/
 cd unep
 python bootstrap.py
+echo
+echo "=======================Setup links to dev trunks"
+ln -sf ~/repos/google/project-database/unep.project-database/trunk products/ProjectDatabase
+ln -sf ~/repos/google/project-database/3rdparty/DataGridField/branches/calendarcolumn products/DataGridField
+ln -sf ~/repos/google/project-database/unep.policy/trunk/unep.policy src/unep.policy
+ln -sf ~/repos/google/project-database/unep.theme/trunk/unep.theme src/unep.theme
+echo
+echo "=======================run buildout"
+./bin/buildout -v
