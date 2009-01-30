402bd55646cf8dc1a31c07e5bb4c632d606a009c
mikejmets <mikejmets@61ed036f-b72b-0410-9ea5-b9ec1d72d98d>
2009-01-30 07:55:29 +0000

    Added variables to build-site.sh

--- buildout/build-script.sh
+++ buildout/build-script.sh
@@ -5,12 +5,16 @@
 #   instances folder (WITHOUT a folder called unep)
 #   PIL is untarred in src/Imaging-1.1.6 
 
+REPOS=~/repos/google/project-database
+INSTANCE=~/instances
+SRC=~/src
+
 echo
 echo "=======================Setup python"
-cd ~/instances
+cd $INSTANCE
 virtualenv --no-site-packages unep
 source unep/bin/activate
-python ~/src/Imaging-1.1.6/setup.py install
+python $SRC/Imaging-1.1.6/setup.py install
 echo
 echo "=======================Setup plone"
 easy_install ZopeSkel
@@ -18,15 +22,15 @@ paster create -t plone3_buildout unep
 echo
 echo "=======================Setup buildout"
 rm unep/buildout.cfg
-ln -s ~/repos/google/project-database/buildout/develop/buildout.cfg unep/
+ln -s $REPOS/buildout/develop/buildout.cfg unep/
 cd unep
 python bootstrap.py
 echo
 echo "=======================Setup links to dev trunks"
-ln -sf ~/repos/google/project-database/unep.project-database/trunk products/ProjectDatabase
-ln -sf ~/repos/google/project-database/3rdparty/DataGridField/branches/calendarcolumn products/DataGridField
-ln -sf ~/repos/google/project-database/unep.policy/trunk/unep.policy src/unep.policy
-ln -sf ~/repos/google/project-database/unep.theme/trunk/unep.theme src/unep.theme
+ln -sf $REPOS/unep.project-database/trunk products/ProjectDatabase
+ln -sf $REPOS/3rdparty/DataGridField/trunk products/DataGridField
+ln -sf $REPOS/unep.policy/trunk/unep.policy src/unep.policy
+ln -sf $REPOS/unep.theme/trunk/unep.theme src/unep.theme
 echo
 echo "=======================run buildout"
 ./bin/buildout -v
