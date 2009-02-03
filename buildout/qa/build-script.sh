fac8347e37bfe8473d0e43e4e5d2b6b8b1f40594
jurgen.blignaut <jurgen.blignaut@61ed036f-b72b-0410-9ea5-b9ec1d72d98d>
2009-02-03 15:13:06 +0000

    Created tags for qa release on 2009-02-03

---
+++ buildout/qa/build-script.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#Assumptions:
+# The current user home folder contains:
+#   repos/google/project-database/...
+#   instances folder (WITHOUT a folder called unep)
+#   PIL is untarred in src/Imaging-1.1.6 
+
+REPOS=~/repos/google/project-database
+INSTANCE=~/instances
+SRC=~/src
+
+echo
+echo "=======================Setup python"
+cd $INSTANCE
+virtualenv --no-site-packages unep
+source unep/bin/activate
+python $SRC/Imaging-1.1.6/setup.py install
+echo
+echo "=======================Setup plone"
+easy_install ZopeSkel
+paster create -t plone3_buildout unep
+echo
+echo "=======================Setup buildout"
+rm unep/buildout.cfg
+ln -s $REPOS/buildout/develop/buildout.cfg unep/
+cd unep
+python bootstrap.py
+echo
+echo "=======================Setup links to dev trunks"
+ln -sf $REPOS/unep.project-database/trunk products/ProjectDatabase
+ln -sf $REPOS/3rdparty/DataGridField/trunk products/DataGridField
+ln -sf $REPOS/unep.policy/trunk/unep.policy src/unep.policy
+ln -sf $REPOS/unep.theme/trunk/unep.theme src/unep.theme
+echo
+echo "=======================run buildout"
+./bin/buildout -v
