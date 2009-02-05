82c149e6a3fc5b658a41e6a804f25144db433d91
isburger <isburger@61ed036f-b72b-0410-9ea5-b9ec1d72d98d>
2009-02-05 13:37:16 +0000

    * implemented dummy pdf and csv options

--- unep.project-database/trunk/browser/reports/dummyreport.py
+++ unep.project-database/trunk/browser/reports/dummyreport.py
@@ -9,7 +9,29 @@ from Products.ProjectDatabase.reports.DummyReportFactory import getDummyReport
 class DummyReport(BrowserView):
     implements(IBrowserView)
 
-    __call__ = ViewPageTemplateFile("simplehtmlreport.pt")
+    pt = ViewPageTemplateFile("simplehtmlreport.pt")
 
+    def csv(self):
+        report = self.getReport()
+        return "hello,csv,world"
+
+    def pdf(self):
+        return "I am not a pdf, but I can fake it"
+
+    def __call__(self, format=None):
+        if format is None:
+            format = self.request.get('format', None)
+
+        if format == 'csv':
+            return self.csv()
+
+        if format == 'pdf':
+            return self.pdf()
+
+        return self.pt()
+        
     def getReport(self):
         return getDummyReport(self.context)
+
+    def getCSVReport(self):
+        return self.__call__(format="csv")
