44794e47a35a10ece6e43f778686556d25d5b0b9
isburger <isburger@61ed036f-b72b-0410-9ea5-b9ec1d72d98d>
2009-02-06 09:56:33 +0000

    * simple way to bring in a compatible reportlab, we use the debian package which already has the font issue solved.

--- buildout/develop/build-script.sh
+++ buildout/develop/build-script.sh
@@ -3,7 +3,8 @@
 # The current user home folder contains:
 #   repos/google/project-database/...
 #   instances folder (WITHOUT a folder called unep)
-#   PIL is untarred in src/Imaging-1.1.6 
+#   PIL is untarred in src/Imaging-1.1.6
+#   python-reportlab is already installed in the system python
 
 REPOS=~/repos/google/project-database
 INSTANCE=~/instances
@@ -15,6 +16,8 @@ cd $INSTANCE
 virtualenv --no-site-packages unep
 source unep/bin/activate
 python $SRC/Imaging-1.1.6/setup.py install
+echo "=======================Setup Reportlab"
+ln -s /usr/lib/python2.4/site-packages/reportlab unep/lib/python2.4/site-packages/reportlab
 echo
 echo "=======================Setup plone"
 easy_install ZopeSkel
