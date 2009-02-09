a23a1bf2576541fd40f4cada3ee149c27f9fe4ef
isburger <isburger@61ed036f-b72b-0410-9ea5-b9ec1d72d98d>
2009-02-09 12:23:25 +0000

    * another skeleton

---
+++ unep.project-database/trunk/reports/qoterrplReportFactory.py
@@ -0,0 +1,22 @@
+from Report import Report
+
+class qoterrplReportFactory(object):
+
+    def __init__(self, context, **kw):
+        self.context = context
+        self.params = kw
+
+    def getReport(self, name):
+        # create and fill the report
+        report = Report(name)
+        report.setReportHeaders(( name,),)
+        report.setTableHeaders(((
+            'Project title',
+            'EOU rating',
+            'GEF EO Rating',
+            ),))
+        # XXX Implement this
+        # report.setTableRows()
+        # report.setTableTotals([])
+        # report.setReportFooters()
+        return report
