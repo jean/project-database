f44b9dba44adc7c5080002e7df2c8d9983698a58
isburger <isburger@61ed036f-b72b-0410-9ea5-b9ec1d72d98d>
2009-02-09 13:08:02 +0000

    * more skeleton report builders

---
+++ unep.project-database/trunk/reports/InternallyExecutedProjectsReportFactory.py
@@ -0,0 +1,24 @@
+from Report import Report
+
+class InternallyExecutedProjectsReportFactory(object):
+
+    def __init__(self, context, **kw):
+        self.context = context
+        self.params = kw
+
+    def getReport(self, name):
+        # create and fill the report
+        report = Report(name)
+        report.setReportHeaders(( name,),)
+        report.setTableHeaders(((
+            'Division',
+            'Project title',
+            'Focal Area',
+            'Status',
+            'GEF amount',
+            ),))
+        # XXX Implement this
+        # report.setTableRows()
+        # report.setTableTotals([])
+        # report.setReportFooters()
+        return report
