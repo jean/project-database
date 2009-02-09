76015f1e103cd0a58487f2e7cd14db5820cc7e02
isburger <isburger@61ed036f-b72b-0410-9ea5-b9ec1d72d98d>
2009-02-09 12:44:07 +0000

    * more skeleton reports

---
+++ unep.project-database/trunk/reports/ExecutingAgencyRisk.py
@@ -0,0 +1,22 @@
+from Report import Report
+
+class ExecutingAgencyRisk(object):
+
+    def __init__(self, context, **kw):
+        self.context = context
+        self.params = kw
+
+    def getReport(self, name):
+        # create and fill the report
+        report = Report(name)
+        report.setReportHeaders(( name,),)
+        report.setTableHeaders(((
+            'Executing Agency',
+            'Year',
+            'Risk Ratings',
+            ),))
+        # XXX Implement this
+        # report.setTableRows()
+        # report.setTableTotals([])
+        # report.setReportFooters()
+        return report
