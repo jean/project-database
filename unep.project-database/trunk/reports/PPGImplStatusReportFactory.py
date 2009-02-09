ff816111a15c24a0c1e8161d901cc3db6505f63c
isburger <isburger@61ed036f-b72b-0410-9ea5-b9ec1d72d98d>
2009-02-09 09:44:34 +0000

    * refactored PPGImplStatusReportFactory into a base class FMIImplStatusReportFactory * implemented MSP, FSP and EEA report factories off FMI base class * implemented reports for ppg, msp, fsp and eea

--- unep.project-database/trunk/reports/PPGImplStatusReportFactory.py
+++ unep.project-database/trunk/reports/PPGImplStatusReportFactory.py
@@ -1,56 +1,7 @@
 from Report import Report
+from FMIImplStatusReportFactory import FMIImplementationStatusReportFactory
 
-class PPGImplementationStatusReportFactory(object):
-
-    def __init__(self, projectdatabase, **kw):
-        self.projectdatabase = projectdatabase
-        self.params = kw
+class PPGImplementationStatusReportFactory(FMIImplementationStatusReportFactory):
 
     def getReport(self):
-        # create and fill the report
-        report = Report('PPG Implementation Status Report')
-        report.setReportHeaders((
-            'PPG Implementation Status Report',
-            ),)
-        report.setTableHeaders(((
-            'IMIS No.',
-            'Project Title',
-            'Executing Agency',
-            'Start Date',
-            'Expected Completion Date',
-            'Revised Completion Date',
-            'GEF Amount',
-            'Total Disbursements',
-            'Total Expenditures',
-            'Last Expenditure Report',
-            'Last Progress Report',
-            ),))
-        report.setTableRows(self.getReportData())
-        # report.setTableTotals([])
-        # report.setReportFooters()
-        return report
-
-    def getReportData(self):
-        projects = self.projectdatabase.objectValues(spec='Project')
-        result = []
-        for project in projects:
-            ppg = project.fmi_folder.get('ppg', None)
-            if ppg is not None:
-                result.append((
-                    ppg.getIMISNumber(),
-                    project.Title(),
-                    project.project_general_info.getExecutingAgencyNames(),
-                    'Unknown',
-                    ppg.getExpectedCompletionDate(),
-                    ppg.getRevisedCompletionDate(),
-                    ppg.getSumFinanceObjectAmount(),
-                    ppg.getSumCashDisbursements(),
-                    ppg.getSumYearlyExpenditures(),
-                    ppg.getTotalGEFAmount(),
-                    ppg.getLatestReportData('expenditure', 'report_received_date'),
-                    ppg.getLatestReportData('progress', 'report_received_date'),
-                    ))
-        return result
-
-
-
+        return FMIImplementationStatusReportFactory.getReport(self, 'PPG Implementation Status Report', 'ppg')
