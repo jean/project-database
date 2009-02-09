ffb67a1f86737366e2300d4b8b5796a51612a6db
jurgen.blignaut <jurgen.blignaut@61ed036f-b72b-0410-9ea5-b9ec1d72d98d>
2009-02-09 15:09:45 +0000

    Added report factories

--- unep.project-database/trunk/reports/ProjectsDueForClosureReportFactory.py
+++ unep.project-database/trunk/reports/ProjectsDueForClosureReportFactory.py
@@ -24,8 +24,67 @@ class ProjectsDueForClosureReportFactory(object):
             'Terminal Evaluation',
             ),))
         # XXX Implement this
-        # report.setTableRows()
+        report.setTableRows(self.getReportData())
         # report.setTableTotals([])
         # report.setReportFooters()
         return report
 
+    def getReportData(self):
+        projects = self.projectdatabase.objectValues(spec='Project')
+        result = []
+        for project in projects:
+
+            for fo in project.fmi_folder:
+
+                if fo.getFinanceCategory() == 'ppg' and \
+                        project.milestones.getPPGImplementationDates('CompletionActual') and \
+                        not project.milestones.getPPGImplementationDates('ClosureActual'):
+                    result.append((
+                        fo.getIMISNumber(),
+                        project.Title(),
+                        fo.getId(),
+                        project.milestones.getPPGImplementationDates('CompletionActual'),
+                        'Unknown',
+                        fo.getLatestReportData('terminal', 'report_received_date'),
+                        fo.getLatestReportData('expenditure', 'report_received_date'),
+                        fo.getLatestReportData('audit', 'report_received_date'),
+                        fo.getLatestReportData('inventory', 'report_received_date'),
+                        fo.getLatestReportData('cofinance', 'report_received_date'),
+                        project.milestones.getEvaluationDatesDate('TerminalEvaluationActual')
+                        ))
+
+                elif fo.getFinanceCategory() in ['eea', 'fsp', 'msp'] and \
+                        project.milestones.getProjectImplementationDates('CompletionActual') and \
+                        not project.milestones.getProjectImplementationDates('ClosureActual'):
+                    result.append((
+                        fo.getIMISNumber(),
+                        project.Title(),
+                        fo.getId(),
+                        project.milestones.getProjectImplementationDates('CompletionActual'),
+                        'Unknown',
+                        fo.getLatestReportData('terminal', 'report_received_date'),
+                        fo.getLatestReportData('expenditure', 'report_received_date'),
+                        fo.getLatestReportData('audit', 'report_received_date'),
+                        fo.getLatestReportData('inventory', 'report_received_date'),
+                        fo.getLatestReportData('cofinance', 'report_received_date'),
+                        project.milestones.getEvaluationDatesDate('TerminalEvaluationActual')
+                        ))
+
+                elif fo.getFinanceCategory() in ['phase', 'addon', 'tranche'] and \
+                        project.milestones.getNewPhaseImplementationDate('Completion') and \
+                        not project.milestones.getNewPhaseImplementationDate('Closure'):
+                    result.append((
+                        fo.getIMISNumber(),
+                        project.Title(),
+                        fo.getId(),
+                        project.milestones.getNewPhaseImplementationDate('Completion'),
+                        'Unknown',
+                        fo.getLatestReportData('terminal', 'report_received_date'),
+                        fo.getLatestReportData('expenditure', 'report_received_date'),
+                        fo.getLatestReportData('audit', 'report_received_date'),
+                        fo.getLatestReportData('inventory', 'report_received_date'),
+                        fo.getLatestReportData('cofinance', 'report_received_date'),
+                        project.milestones.getEvaluationDatesDate('TerminalEvaluationActual')
+                        ))
+
+        return result
