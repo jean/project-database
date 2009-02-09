2194a85bf0150a4ada45887a8a82d5df758e18c3
isburger <isburger@61ed036f-b72b-0410-9ea5-b9ec1d72d98d>
2009-02-09 12:14:53 +0000

    * new skeleton class

---
+++ unep.project-database/trunk/reports/MidTermEvaluationStatusReportFactory.py
@@ -0,0 +1,31 @@
+from Report import Report
+
+class MidTermEvaluationStatusReportFactory(object):
+
+    def __init__(self, context, **kw):
+        self.context = context
+        self.params = kw
+
+    def getReport(self, name):
+        # create and fill the report
+        report = Report(name)
+        report.setReportHeaders(( name,),)
+        report.setTableHeaders(((
+			'IMIS No.',
+			'Project title',
+			'Focal area',
+			'Evaluation budget',
+			'TM',
+			'FMO',
+			'MTE Status',
+			'Lead Evaluator',
+			'email',
+			'Phone No.',
+			'Other Evaluators',
+			'Project Rating',
+            ),))
+        # XXX Implement this
+        # report.setTableRows()
+        # report.setTableTotals([])
+        # report.setReportFooters()
+        return report
