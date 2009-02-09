4198becc1e085c607e597241d510f5117d441b8e
isburger <isburger@61ed036f-b72b-0410-9ea5-b9ec1d72d98d>
2009-02-09 10:30:27 +0000

    * refactored into base class

--- unep.project-database/trunk/reports/PPGReceivablesReportFactory.py
+++ unep.project-database/trunk/reports/PPGReceivablesReportFactory.py
@@ -1,44 +1,6 @@
+from FMIReceivablesReportFactory import FMIReceivablesReportFactory
 from Report import Report
 
-class PPGReceivablesReportFactory(object):
-
-    def __init__(self, projectdatabase, **kw):
-        self.projectdatabase = projectdatabase
-        self.params = kw
-
+class PPGReceivablesReportFactory(FMIReceivablesReportFactory):
     def getReport(self):
-        # create and fill the report
-        report = Report('PPG Recievables Report')
-        report.setReportHeaders((
-            'PPG Recievables Report',
-            ),)
-        report.setTableHeaders(((
-            'IMIS No.',
-            'Project Title',
-            'Executing Agency',
-            'GEF Grant',
-            'Total Disbursements',
-            'Total Expenditures',
-            'Receivable/(Payable)',
-            ),))
-        report.setTableRows(self.getReportData())
-        # report.setTableTotals([])
-        # report.setReportFooters()
-        return report
-
-    def getReportData(self):
-        projects = self.projectdatabase.objectValues(spec='Project')
-        result = []
-        for project in projects:
-            ppg = project.fmi_folder.get('ppg', None)
-            if ppg is not None:
-                result.append((
-                    ppg.getIMISNumber(),
-                    project.Title(),
-                    project.project_general_info.getExecutingAgencyNames(),
-                    ppg.getCommittedGEFGrant(),
-                    ppg.getSumCashDisbursements(),
-                    ppg.getSumYearlyExpenditures(),
-                    ppg.getAmountReceivable(),
-                    ))
-        return result
+        return FMIReceivablesReportFactory.getReport(self, 'PPG Receivables Report', 'ppg')
