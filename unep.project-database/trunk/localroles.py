56fe15347b9261c2f83dcf9f14fcd2f76bfda852
isburger <isburger@61ed036f-b72b-0410-9ea5-b9ec1d72d98d>
2009-02-16 09:28:40 +0000

    * added additional missing roles

--- unep.project-database/trunk/localroles.py
+++ unep.project-database/trunk/localroles.py
@@ -3,11 +3,65 @@ from plone.app.workflow.interfaces import ISharingPageRole
 from Products.CMFPlone import PloneMessageFactory as _
 
 class FMORole(object):
+    """
+    Role for Financial Management Officer
+    """
     implements(ISharingPageRole)
     title = _(u'title_fmo_role', u'FMO')
     required_permission = 'Manage portal content'
 
 class MORole(object):
+    """
+    Role for Monitoring Officer
+    """
     implements(ISharingPageRole)
     title = _(u'title_mo_role', u'MO')
+    required_permission = 'Manage portal content'
+
+class EORole(object):
+    """
+    Role for Evaluation Officer
+    """
+    implements(ISharingPageRole)
+    title = _(u'title_eo_role', u'EO')
+    required_permission = 'Manage portal content'
+
+class RegistrarRole(object):
+    """
+    Role for Registrar
+    """
+    implements(ISharingPageRole)
+    title = _(u'title_registrar_role', u'Registrar')
+    required_permission = 'Manage portal content'
+
+class TMRole(object):
+    """
+    Role for Task Manager
+    """
+    implements(ISharingPageRole)
+    title = _(u'title_tm_role', u'TM')
+    required_permission = 'Manage portal content'
+
+class SPORole(object):
+    """
+    Role for Senior Programme Officer
+    """
+    implements(ISharingPageRole)
+    title = _(u'title_spo_role', u'SPO')
+    required_permission = 'Manage portal content'
+
+class SPMORole(object):
+    """
+    Role for <Unknown TLA>
+    """
+    implements(ISharingPageRole)
+    title = _(u'title_spmo_role', u'SPMO')
+    required_permission = 'Manage portal content'
+
+class SMRole(object):
+    """
+    Role for Senior Manager
+    """
+    implements(ISharingPageRole)
+    title = _(u'title_sm_role', u'SM')
     required_permission = 'Manage portal content'
