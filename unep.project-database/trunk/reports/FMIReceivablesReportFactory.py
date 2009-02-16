a35359723748fb9dcb5f6792dc067375f1d7d548
mikejmets <mikejmets@61ed036f-b72b-0410-9ea5-b9ec1d72d98d>
2009-02-16 09:27:24 +0000

    added strip to FMIReceivable amounts

--- unep.project-database/trunk/reports/FMIReceivablesReportFactory.py
+++ unep.project-database/trunk/reports/FMIReceivablesReportFactory.py
@@ -1,4 +1,5 @@
 from Report import Report
+from Products.ProjectDatabase.utils import inner_strip
 
 class FMIReceivablesReportFactory(object):
 
@@ -32,12 +33,12 @@ class FMIReceivablesReportFactory(object):
             if ob is not None:
                 result.append((
                     ob.getIMISNumber(),
-                    project.Title(),
+                    project.project_general_info.Title(),
                     project.project_general_info.getExecutingAgencyNames(),
-                    ob.getCommittedGEFGrant(),
-                    ob.getSumCashDisbursements(),
-                    ob.getSumYearlyExpenditures(),
-                    ob.getAmountReceivable(),
+                    inner_strip(ob.getCommittedGEFGrant()),
+                    inner_strip(ob.getSumCashDisbursements()),
+                    inner_strip(ob.getSumYearlyExpenditures()),
+                    inner_strip(ob.getAmountReceivable()),
                     ))
         return result
 
