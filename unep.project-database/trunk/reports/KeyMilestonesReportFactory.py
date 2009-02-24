b63964008bbf95606850c63d7e3aa4545189062d
mikejmets <mikejmets@61ed036f-b72b-0410-9ea5-b9ec1d72d98d>
2009-02-24 07:53:50 +0000

    Added countryclasssys view

--- unep.project-database/trunk/reports/KeyMilestonesReportFactory.py
+++ unep.project-database/trunk/reports/KeyMilestonesReportFactory.py
@@ -1,4 +1,5 @@
 from Report import Report
+from DateTime import DateTime
 
 class KeyMilestonesReportFactory(object):
 
@@ -10,11 +11,34 @@ class KeyMilestonesReportFactory(object):
         # create and fill the report
         name = "Key Milestones Report"
         report = Report(name)
-        report.setReportHeaders(( name,),)
-        # XXX Implement this
-        #report.setTableHeaders(((
-        #    ),))
-        # report.setTableRows()
+        report.setReportHeaders(( \
+            name, 
+            "Project Title: %s" % self.context.pgi.Title(),
+            "Report Date: %s" % DateTime()),)
+        report.setTableRows(self.getReportData())
         # report.setTableTotals([])
         # report.setReportFooters()
         return report
+
+    def getReportData(self):
+        project = self.context
+        result = []
+        pgi = project.project_general_info
+        ms = project.milestones
+        mofu = project.fmi_folder.getMainFinanceObject()
+        if mofu and project.isTheProjectPublished():
+            result.append((
+                unep_report_format_date(self.projectdatabase, \
+                    ms.getProjectApprovalDate('PRCReview')),
+                unep_report_format_date(self.projectdatabase, \
+                    ms.getProjectApprovalDate('CEOApprovalEndorsementExpected')),
+                unep_report_format_date(self.projectdatabase, \
+                    ms.getProjectApprovalDate('SubmissionToGEFSec')),
+                unep_report_format_date(self.projectdatabase, \
+                    ms.getProjectApprovalDate('ReviewSheet')),
+                unep_report_format_date(self.projectdatabase, \
+                    ms.getProjectApprovalDate('CEOApprovalEndorsementActual')),
+                unep_report_format_date(self.projectdatabase, \
+                    ms.getProjectApprovalDate('PAGApprovalExpected')),
+                ))
+        return result
--- unep.project-database/trunk/reports/qoterrReportFactory.py
+++ unep.project-database/trunk/reports/qoterrReportFactory.py
@@ -17,25 +17,23 @@ class qoterrReportFactory(object):
             'EOU rating',
             'GEF EO Rating',
             ),))
-        # XXX Implement this
         report.setTableRows(self.getReportData())
         # report.setTableTotals([])
         # report.setReportFooters()
         return report
 
     def getReportData(self):
-        projects = self.context.objectValues(spec='Project')
         result = []
-        for project in projects:
-            pgi = project.project_general_info
-            mande = project.mne_folder
-            for me in mande.objectValues(spec='MonitoringAndEvaluation'):
-                if me.getEvaluationType() == 'TE':
-                    result.append((
-                        pgi.Title(),
-                        getVocabularyValue(self.context, \
-                            'Rating', me.getEOUTerminalEvaluationReportQuality()),
-                        getVocabularyValue(self.context, \
-                            'Rating', me.getGEFTerminalEvaluationReportQuality())
-                        ))
+        project = self.context
+        pgi = project.project_general_info
+        mande = project.mne_folder
+        for me in mande.objectValues(spec='MonitoringAndEvaluation'):
+            if me.getEvaluationType() == 'TE':
+                result.append((
+                    pgi.Title(),
+                    getVocabularyValue(self.context, \
+                        'Rating', me.getEOUTerminalEvaluationReportQuality()),
+                    getVocabularyValue(self.context, \
+                        'Rating', me.getGEFTerminalEvaluationReportQuality())
+                    ))
         return result
