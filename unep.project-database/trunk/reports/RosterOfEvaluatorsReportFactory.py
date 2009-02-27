adfd013f60f4d0576fde680c72929c4772af020f
mikejmets <mikejmets@61ed036f-b72b-0410-9ea5-b9ec1d72d98d>
2009-02-27 12:57:28 +0000

    IMplemented Roster report

--- unep.project-database/trunk/reports/RosterOfEvaluatorsReportFactory.py
+++ unep.project-database/trunk/reports/RosterOfEvaluatorsReportFactory.py
@@ -24,8 +24,33 @@ class RosterOfEvaluatorsReportFactory(object):
             'Cell Phone',
             'Phone No.',
             ),))
-        # XXX Implement this
-        # report.setTableRows()
+        report.setTableRows(self.getReportData())
         # report.setTableTotals([])
         # report.setReportFooters()
         return report
+
+    def getReportData(self):
+        projects = self.context.objectValues(spec='Project')
+        result = []
+        for project in projects:
+            if project.isTheProjectPublished():
+                pgi = project.project_general_info
+                mnes = \
+                    project.mne_folder.objectValues(spec='MonitoringAndEvaluation')
+                for mne in mnes:
+                    evaluator, nationality, expertise, email, cell, phone = \
+                        mne.getLeadEvaluatorDetails()
+                    result.append((
+                        pgi.Title(),
+                        pgi.getFocalAreaNames(),
+                        mne.getEvaluationType(),
+                        mne.getEOUTerminalEvaluationReportQuality(),
+                        "Unknown",
+                        evaluator, 
+                        nationality, 
+                        expertise, 
+                        email, 
+                        cell, 
+                        phone,
+                        ))
+        return result
