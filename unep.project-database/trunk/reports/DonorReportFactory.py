99ef2a0b5acaa1a019e0bedd8202b02ef495d535
mikejmets <mikejmets@61ed036f-b72b-0410-9ea5-b9ec1d72d98d>
2009-02-27 13:44:52 +0000

    Completed Donor report

--- unep.project-database/trunk/reports/DonorReportFactory.py
+++ unep.project-database/trunk/reports/DonorReportFactory.py
@@ -1,4 +1,5 @@
 from Report import Report
+from Products.ProjectDatabase.utils import inner_strip
 
 class DonorReportFactory(object):
 
@@ -23,8 +24,31 @@ class DonorReportFactory(object):
             'Total project grant',
             'Project status',
             ),))
-        # XXX Implement this
-        # report.setTableRows()
+        report.setTableRows(self.getReportData())
         # report.setTableTotals([])
         # report.setReportFooters()
         return report
+
+    def getReportData(self):
+        projects = self.context.objectValues(spec='Project')
+        result = []
+        for project in projects:
+            mofu = project.fmi_folder.getMainFinanceObject()
+            pgi = project.project_general_info
+            if mofu and project.isTheProjectPublished():
+                pledges = mofu.getDonorPledges()
+                for pledge in pledges:
+                    result.append((
+                        pledge['name'],
+                        pledge['type'],
+                        pgi.Title(),
+                        pgi.getFocalAreaNames(),
+                        pgi.getGeographicScopeValues(),
+                        pgi.getRegionNames(),
+                        pgi.getCountryNames(),
+                        inner_strip(pledge['amount']),
+                        inner_strip(mofu.getSumFinanceObjectAmount()),
+                        mofu.getStatus(),
+                        ))
+        return result
+
