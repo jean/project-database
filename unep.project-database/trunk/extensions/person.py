98f2a1820a701a0ac95874b332475c61a6311478
mikejmets <mikejmets@61ed036f-b72b-0410-9ea5-b9ec1d72d98d>
2009-03-04 13:06:03 +0000

    Adde more extended schema fields

--- unep.project-database/trunk/extensions/person.py
+++ unep.project-database/trunk/extensions/person.py
@@ -4,12 +4,51 @@ from zope.interface import implements
 from archetypes.schemaextender.interfaces import IOrderableSchemaExtender
 from Products.Archetypes.atapi import *
 from Products.UpfrontContacts.Person import Person
+from Products.ATVocabularyManager.namedvocabulary import NamedVocabulary
+from Products.ProjectDatabase.widgets.UNEPSelectionWidget \
+        import UNEPSelectionWidget
 from utils import *
+from Products.CMFCore.utils import getToolByName
+
+class RatingComputedField(ExtensionField, ComputedField):
+    """ """
+    def getRating(self, context):
+        """ """
+        print "Inside Last Rating"
+        refcat = getToolByName(context, 'reference_catalog')
+        mnes = refcat.getReferences(context.UID())
+        import pdb; pdb.set_trace()
+        return 'KAK'
+
+
 
 fields = [
+        efStringField("Salutation", 
+            widget = UNEPSelectionWidget(
+                label=_(u'label_salutation', default=u'Salutation')),
+            vocabulary=NamedVocabulary("""Salutation"""),
+        ),
         efStringField("JobTitle", 
-            widget = StringWidget(
+            widget = UNEPSelectionWidget(
                 label=_(u'label_jobtitle', default=u'Job Title')),
+            vocabulary=NamedVocabulary("""JobTitle"""),
+        ),
+        efStringField("FocalArea", 
+            widget = UNEPSelectionWidget(
+                label=_(u'label_focalarea', default=u'Focal Area')),
+            vocabulary=NamedVocabulary("""FocalArea"""),
+        ),
+        efStringField("AreaOfExpertise", 
+            widget = StringWidget(
+                label=_(u'label_areaofexpertise', \
+                        default=u'Area Of Expertise')),
+        ),
+        RatingComputedField("LastRating", 
+            expression="context.Schema().getField('LastRating').getRating(context)",
+            widget = ComputedField._properties['widget'](
+                visible=True,
+                label=_(u'label_lastrating', \
+                        default=u'Last Rating')),
         ),
         ]
 
