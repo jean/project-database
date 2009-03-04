811aa20692f80f2eb5f290119b683f7144211b58
jurgen.blignaut <jurgen.blignaut@61ed036f-b72b-0410-9ea5-b9ec1d72d98d>
2009-03-04 09:50:59 +0000

    Added schemaextender utils

---
+++ unep.project-database/trunk/extensions/utils.py
@@ -0,0 +1,29 @@
+"""
+Helper classes and function to save us from tedious copy and paste 
+work when extending AT schemas with schemaextender.
+"""
+from Products.Archetypes.atapi import *
+from Products.Archetypes.Field import __all__ as ATField_all
+from Products.DataGridField import DataGridField
+from archetypes.schemaextender.field import ExtensionField
+from copy import deepcopy
+from Products.ATBackRef.BackReferenceField import BackReferenceField
+
+def _at_field_copy(field, name=None):
+        """
+        Same as Archetypes.Field except for last line of method
+        """
+        cdict = dict(vars(field))
+        cdict.pop('__name__')
+        # Widget must be copied separatedly
+        widget = cdict['widget']
+        del cdict['widget']
+        properties = deepcopy(cdict)
+        properties['widget'] = widget.copy()
+        name = name is not None and name or field.getName()
+        properties['name'] = name
+        return properties
+
+ATField_all = ATField_all + ('DataGridField','BackReferenceField')
+for klass in ATField_all:
+    exec("class ef%s(ExtensionField, %s): pass" % (klass, klass))
