2126c372d32fdec5ee1baa5900a91e09222b631a
jurgen.blignaut <jurgen.blignaut@61ed036f-b72b-0410-9ea5-b9ec1d72d98d>
2009-03-05 15:39:42 +0000

    Added filtered search to reports

--- unep.project-database/trunk/browser/reports/ipistatusreport.py
+++ unep.project-database/trunk/browser/reports/ipistatusreport.py
@@ -1,7 +1,14 @@
 from Products.ProjectDatabase.reports.IPIStatusReportFactory import IPIStatusReportFactory
 from basereport import BaseReport
+from Products.CMFCore.utils import getToolByName
 
 class IPIStatusReport(BaseReport):
     def getReport(self):
-        factory = IPIStatusReportFactory(self.context)
+        rc = getToolByName(self, 'reference_catalog')
+        projects = []
+        UIDs = self.context.REQUEST.get('projects', None)
+        if UIDs:
+            UIDs = UIDs.split('|')
+            projects = [rc.lookupObject(UID) for UID in UIDs]
+        factory = IPIStatusReportFactory(self.context, projects=projects)
         return factory.getReport()
