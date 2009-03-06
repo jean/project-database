eea4f5604469ffcb1ddf48caca2b1ce80e474b1f
mikejmets <mikejmets@61ed036f-b72b-0410-9ea5-b9ec1d72d98d>
2009-03-06 09:23:21 +0000

    Completed Framework report

--- unep.project-database/trunk/reports/ProgrammeFrameworkReportFactory.py
+++ unep.project-database/trunk/reports/ProgrammeFrameworkReportFactory.py
@@ -1,4 +1,6 @@
 from Report import Report
+from Products.ProjectDatabase.utils import inner_strip, unep_report_format_date
+from Products.CMFCore.utils import getToolByName
 
 class ProgrammeFrameworkReportFactory(object):
 
@@ -11,11 +13,59 @@ class ProgrammeFrameworkReportFactory(object):
         name = "Programme Framework Report"
         report = Report(name)
         report.setReportHeaders(( name,),)
-        # XXX Implement this
         #report.setTableHeaders(((
         #    ),))
-        # report.setTableRows()
+        report.setTableRows(self.getReportData())
         # report.setTableTotals([])
         # report.setReportFooters()
         return report
 
+    def getReportData(self):
+        results = []
+        frameworks = self.context. \
+            frameworkdatabase.objectValues(spec='ProgrammeFramework')
+        refcat = getToolByName(self.context, 'reference_catalog')
+        for framework in frameworks:
+            row = []
+            relationships = refcat.getBackReferences(
+                framework.UID(), 
+                relationship='project_programmeframework')
+            row.extend(['Title', framework.Title()])
+            row.extend(['PFD Short Title', framework.getPFDShortTitle()])
+            row.extend(['SPO', framework.getSPOName()])
+            row.extend(['Lead Agency', framework.getLeadAgency()])
+            row.extend(['Deadline', framework.getProjectSubmissionDeadline()])
+            row.append('')
+            results.append(row)
+            row = ('DBase ID', 'GEF ID', 'IMIS No', 'Title', 'Country(ies)', \
+                    'TM', 'FMO', 'PIF Submission Date', \
+                    'Expected CEO Endorsement', \
+                    'Actual CEO Endorsement', 'Status')
+            results.append(row)
+            for rel in relationships:
+                pgi = refcat.lookupObject(rel.sourceUID)
+                project = pgi.getAProject()
+                mofu = project.fmi_folder.getMainFinanceObject()
+                ms = project.milestones
+                if mofu and project.isTheProjectPublished():
+                    results.append((
+                        project.getId(),
+                        pgi.getGEFid(),
+                        mofu.getIMISNumber(),
+                        pgi.Title(),
+                        pgi.getCountryNames(),
+                        pgi.getCurrentTM(),
+                        mofu.getCurrentPrincipalFMO(),
+                        unep_report_format_date(project, \
+                            ms.getPIFApprovalDate('SubmissionToGEFSec')),
+                        unep_report_format_date(project, \
+                            ms.getProjectApprovalDate(\
+                                    'CEOApprovalEndorsementExpected')),
+                        unep_report_format_date(project, \
+                            ms.getProjectApprovalDate( \
+                                    'CEOApprovalEndorsementActual')),
+                        ms.getProjectStage()
+                        ))
+            results.append(['--', '--','--', '--','--', '--','--', '--',\
+                            '--', '--','--'])
+        return results
