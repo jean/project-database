6da97360c1e7aa1e0af716ae25ee65d30dfe017a
jurgen.blignaut <jurgen.blignaut@61ed036f-b72b-0410-9ea5-b9ec1d72d98d>
2009-05-26 14:03:36 +0000

    Bugfixes

--- unep.project-database/trunk/browser/reports/executingagencyriskreport.py
+++ unep.project-database/trunk/browser/reports/executingagencyriskreport.py
@@ -5,5 +5,5 @@ from Products.CMFCore.utils import getToolByName
 
 class ExecutingAgencyRiskReport(BaseReport):
     def getReport(self):
-        factory = ExecutingAgencyRiskReportFactory(self.context, projects=self_projects)
+        factory = ExecutingAgencyRiskReportFactory(self.context, projects=self.projects)
         return factory.getReport()
