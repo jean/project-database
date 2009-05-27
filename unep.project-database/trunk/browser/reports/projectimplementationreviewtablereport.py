3cb4dab73d791bdfd20ad02d40e102b5ca12afa3
jurgen.blignaut <jurgen.blignaut@61ed036f-b72b-0410-9ea5-b9ec1d72d98d>
2009-05-27 09:17:09 +0000

    Rewired configure.zcml for report views to declare template explicitly

--- unep.project-database/trunk/browser/reports/projectimplementationreviewtablereport.py
+++ unep.project-database/trunk/browser/reports/projectimplementationreviewtablereport.py
@@ -1,6 +1,7 @@
 from Products.ProjectDatabase.reports.ProjectImplementationReviewTableReportFactory \
         import ProjectImplementationReviewTableReportFactory
 from basereport import BaseReport
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 
 class ProjectImplementationReviewTableReport(BaseReport):
     def getReport(self):
--- unep.project-database/trunk/browser/reports/qoterrreport.py
+++ unep.project-database/trunk/browser/reports/qoterrreport.py
@@ -1,6 +1,7 @@
 from Products.ProjectDatabase.reports.qoterrReportFactory \
     import qoterrReportFactory
 from basereport import BaseReport
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 
 class qoterrReport(BaseReport):
     def getReport(self):
