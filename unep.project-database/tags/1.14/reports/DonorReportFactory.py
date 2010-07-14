64f6bafb4e8488de346e8da77d0882142856faf2
roche.compaan <roche.compaan@61ed036f-b72b-0410-9ea5-b9ec1d72d98d>
2010-07-14 10:52:12 +0000

    * tag release

---
+++ unep.project-database/tags/1.14/reports/DonorReportFactory.py
@@ -0,0 +1,54 @@
+from Report import Report
+from Products.ProjectDatabase.utils import inner_strip
+
+class DonorReportFactory(object):
+
+    def __init__(self, context, **kw):
+        self.context = context
+        self.params = kw
+
+    def getReport(self):
+        # create and fill the report
+        name = "Donor Report"
+        report = Report(name)
+        report.setReportHeaders(( name,),)
+        report.setTableHeaders(((
+            'Donor name',
+            'Donor type',
+            'Project title',
+            'Focal Area',
+            'Geographic scope',
+            'Region',
+            'Country(ies)',
+            'Pledged (planned) amount',
+            'Total project grant',
+            'Project status',
+            ),))
+        report.setTableRows(self.getReportData())
+        # report.setTableTotals([])
+        # report.setReportFooters()
+        return report
+
+    def getReportData(self):
+        projects = self.params.get('projects', None)
+        result = []
+        for project in projects:
+            mofu = project.fmi_folder.getMainFinanceObject()
+            pgi = project.project_general_info
+            if mofu and project.isTheProjectPublished():
+                pledges = mofu.getDonorPledges()
+                for pledge in pledges:
+                    result.append((
+                        pledge['name'],
+                        pledge['type'],
+                        pgi.Title(),
+                        pgi.getFocalAreaNames(),
+                        pgi.getGeographicScopeValues(),
+                        pgi.getRegionNames(),
+                        pgi.getCountryNames(),
+                        inner_strip(pledge['amount']),
+                        inner_strip(mofu.getSumFinanceObjectAmount()),
+                        mofu.getStatus(),
+                        ))
+        return result
+
