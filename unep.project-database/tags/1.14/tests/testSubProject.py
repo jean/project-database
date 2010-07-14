e8db68184ab771876584499abd45177f5b5550da
roche.compaan <roche.compaan@61ed036f-b72b-0410-9ea5-b9ec1d72d98d>
2010-07-14 10:52:12 +0000

    * tag release

---
+++ unep.project-database/tags/1.14/tests/testSubProject.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+#
+# File: testSubProject.py
+#
+# Copyright (c) 2009 by []
+# Generator: ArchGenXML Version 2.1
+#            http://plone.org/products/archgenxml
+#
+# GNU General Public License (GPL)
+#
+
+__author__ = """Mike Metcalfe <mikejmets@gmail.com>, Jurgen Blignaut
+<jurgen.blignaut@gmail.com>"""
+__docformat__ = 'plaintext'
+
+import os, sys
+if __name__ == '__main__':
+    execfile(os.path.join(sys.path[0], 'framework.py'))
+
+##code-section module-header #fill in your manual code here
+##/code-section module-header
+
+#
+# Test-cases for class(es) 
+#
+
+from Testing import ZopeTestCase
+from Products.ProjectDatabase.config import *
+from Products.ProjectDatabase.tests.testPortalDatabase import testPortalDatabase
+
+# Import the tested classes
+from Products.ProjectDatabase.content.SubProject import SubProject
+
+##code-section module-beforeclass #fill in your manual code here
+##/code-section module-beforeclass
+
+
+class testSubProject(testPortalDatabase):
+    """Test-cases for class(es) ."""
+
+    ##code-section class-header_testSubProject #fill in your manual code here
+    ##/code-section class-header_testSubProject
+
+    def afterSetUp(self):
+        pass
+
+    # Manually created methods
+
+
+def test_suite():
+    from unittest import TestSuite, makeSuite
+    suite = TestSuite()
+    suite.addTest(makeSuite(testSubProject))
+    return suite
+
+##code-section module-footer #fill in your manual code here
+##/code-section module-footer
+
+if __name__ == '__main__':
+    framework()
+
+
