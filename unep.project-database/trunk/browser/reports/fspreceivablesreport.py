9ce4c9ba173d601e4e27c0967e2e7aa179a204a8
rijk.stofberg <rijk.stofberg@61ed036f-b72b-0410-9ea5-b9ec1d72d98d>
2010-07-23 14:08:45 +0000

    Refactor to find corresponding subprojects too and exclude those projects which have no fsp finance objects.

--- unep.project-database/trunk/browser/reports/fspreceivablesreport.py
+++ unep.project-database/trunk/browser/reports/fspreceivablesreport.py
@@ -4,5 +4,15 @@ from Products.CMFCore.utils import getToolByName
 
 class FSPReceivablesReport(BaseReport):
     def getReport(self):
+        # Since this is an FSP report and FSP finance objects can contain
+        # subprojects, we extend the _projects property with those.
+        projects = []
+        for project in self._projects:
+            fmi = project.getFMIFolder()
+            if 'fsp' in fmi.objectIds():
+                projects.append(project)
+                sub_projects = fmi.getFspSubProjects()
+                projects.extend(sub_projects)
+        self._projects = projects
         factory = FSPReceivablesReportFactory(self.context, projects=self._projects)
         return factory.getReport()
